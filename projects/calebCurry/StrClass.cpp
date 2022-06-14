93210c3660cb47ad240f68eacfb124b9eb42cd93
Richm <ric5hme45@yahoo.com>
2022-06-14 11:15:47 +0300

    21548

---
+++ projects/calebCurry/StrClass.cpp
@@ -0,0 +1,19 @@
+ /*   
+ C++ Programming Tutorial 24 - String Class and C Strings
+ StrClass.cpp
+ g++ StrClass.cpp
+ https://www.youtube.com/watch?v=-t07hCkdHjI&list=PL_c9BZzLwBRJVJsIfe97ey45V4LP_HXiG&index=26
+ */
+
+#include <iostream>
+#include <climits>
+#include <cmath>
+
+ 
+using std::cout;
+
+main(){
+    cout << "String Class and C String"<< std::endl;   
+      
+}  
+ 
