f126bf86c2f5bc09cb4520b1960c273c28f2e621
Richm <ric5hme45@yahoo.com>
2023-04-09 15:34:21 +0300

    23739

--- projects/calebCurry/vectors.cpp
+++ projects/calebCurry/vectors.cpp
@@ -16,15 +16,28 @@ https://www.youtube.com/watch?v=Fm9otubOsqQ&list=PL_c9BZzLwBRJVJsIfe97ey45V4LP_H
 #include <iostream>
 #include <vector>
  
-  
-main(){
 
-  std::vector<int>  data{1,2,3};
+ void print_vector(std::vector<int> data){
+  std::cout <<"\n -------------\n" << "\n";
+   for (int i = 0; i < data.size(); i++){
+       std::cout << data[i] << "\t";
+   } 
+ 
+ } //print_vector
+
+
+int main(){
+
+  std::vector<int>  data{1,2,3,4,5,6};
+  print_vector(data);
   data.push_back(12);
+  print_vector(data);
+  std::cout <<"\n ********-\n" << "\n";
   std::cout << data[1] << std::endl;
   std::cout << data[data.size() -1] << std::endl;
+  std::cout <<"\n ********-\n" << "\n";
   data.pop_back();//Remove last  item
- 
+  print_vector(data);
     
 }//main
    
