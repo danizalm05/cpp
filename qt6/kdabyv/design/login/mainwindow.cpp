7d0f6d6e1c079e29409279f4c1ab737b62ab2329
danizalm05 <danizalm05@gmail.com>
2024-06-09 16:10:20 +0300

    19172

--- qt6/kdabyv/design/login/mainwindow.cpp
+++ qt6/kdabyv/design/login/mainwindow.cpp
@@ -2,8 +2,7 @@
 /*
  * Skills By Mbdia
  Login and Registration Form
-https://www.youtube.com/watch?v=4qjpSH53Zrs&list=PL8MpAn684UHKt_umMjmD0EhbV5IZ-5uLd&index=1
-26:40
+        26:40
 */
 
 
@@ -29,7 +28,7 @@ MainWindow::~MainWindow()
 void MainWindow::button()
 {
     QString name[] = {"abc", "cdf", "ghi"};
-    QString code[] = {"abc12 ", "def354 ", "ghi56"};
+    QString code[] = {"abc12", "def354 ", "ghi56"};
 
     QString uName = ui->lineEdit->text();
     QString uCode = ui->lineEdit_2->text();
@@ -37,14 +36,15 @@ void MainWindow::button()
     for(int i =0; i< name->length(); i++) {
         if(uName == name[i] && uCode == code[i] ){
             QMessageBox::information(this,"Welcome",
-                   "User:" + name[i]+"\n"+
+                   "User: " + name[i]+"\n"+
                  "Welcom to loging system" );
             found = true;
+
             break;
         }
         else if(! found){
             QMessageBox::information(this,"Error",
-                 "Invalid user name or passward!" );
+                 "Invalid   name or passward!" );
 
         }
 
