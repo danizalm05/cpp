269b99dd3dac197cb00e98ef06e86d2b8cbf398f
danizalm05 <danizalm05@gmail.com>
2024-06-15 16:56:55 +0300

    17233

--- qt6/kdabyv/airline/airline/mainwindow.cpp
+++ qt6/kdabyv/airline/airline/mainwindow.cpp
@@ -4,20 +4,51 @@ Airlines Reservation Management System
 Skills By Mbdia
 https://www.youtube.com/watch?v=AGGMr1LoC2M&list=PL8MpAn684UHKt_umMjmD0EhbV5IZ-5uLd&index=3
 
-10:35
+24:05
 */
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->pushButton,SIGNAL(clicked()),
+                 this,SLOT(on_pushButton_clicked()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString flights[] =  {"flg101","flg102","flg103"};
+    int seats[] = {20,50,30};
+    QString uFlight = ui->lineEdit->text();
+    QString uName = ui->lineEdit_2->text();
+
+    for(int i =0;i < flights->length();i++){
+        if(uFlight == flights[i] && seats[i]>0 ){
+            QMessageBox::information(this,"Sucsess",
+                 "User Name: "+uName+"\n"+
+                 "flight:"+ flights[i] );
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
