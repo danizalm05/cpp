57e9a0cefd1cf5c2f487ee0c0d7ec108b697e53a
danizalm05 <danizalm05@gmail.com>
2024-06-20 16:26:57 +0300

    13152

---
+++ projects/calebCurry/01Reference.cpp
@@ -0,0 +1,25 @@
+ /*  
+      Tutorial 1 and 2 - Working with References in C++
+https://www.youtube.com/watch?v=OCL7mSFCIx0&list=PL_c9BZzLwBRJkVDaJbLHrrjNH_phcbCy7
+https://www.youtube.com/watch?v=GVEcLAX7ogk&list=PL_c9BZzLwBRJkVDaJbLHrrjNH_phcbCy7&index=2   
+      1.   save file
+      2.  g++  01Reference.cpp  -o  refe  -std=c++11
+      3.  ./refe
+ ---------------------------
+        debug
+   F5            Start Debugging   
+  shift + F5     exit debug
+  F10            move to next line
+   */
+
+#include <iostream>
+
+
+int main(){
+
+ return 0;
+}//main
+ 
+ 
+  
+  
