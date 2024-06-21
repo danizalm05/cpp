a88a10d630da11079dfe2097acf57a96569fb32f
danizalm05 <danizalm05@gmail.com>
2024-06-21 17:01:47 +0300

    7211

--- projects/calebCurry/01Reference.cpp
+++ projects/calebCurry/01Reference.cpp
@@ -2,6 +2,7 @@
       Tutorial 1 and 2 - Working with References in C++
 https://www.youtube.com/watch?v=OCL7mSFCIx0&list=PL_c9BZzLwBRJkVDaJbLHrrjNH_phcbCy7
 https://www.youtube.com/watch?v=GVEcLAX7ogk&list=PL_c9BZzLwBRJkVDaJbLHrrjNH_phcbCy7&index=2   
+3:44
       1.   save file
       2.  g++  01Reference.cpp  -o  refe  -std=c++11
       3.  ./refe
@@ -13,9 +14,24 @@ https://www.youtube.com/watch?v=GVEcLAX7ogk&list=PL_c9BZzLwBRJkVDaJbLHrrjNH_phcb
    */
 
 #include <iostream>
-
+ 
 
 int main(){
+ int a =10;
+ int& b =a;
+ int c =100;
+ std::cout <<"a =" <<a << std::endl <<"b ="  <<b <<   std::endl;
+ std::cout <<"&a =" <<&a << std::endl <<"&b ="  <<&b <<   std::endl;
+ a = 15;
+ std::cout <<"a = " <<a << std::endl <<"b = "  << b <<   std::endl;
+
+
+
+
+
+
+
+
 
  return 0;
 }//main
