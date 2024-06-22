103be9e7c017cdc4ef07d2190a8d336425ac8d58
Richm <ric5hme45@yahoo.com>
2024-06-22 13:00:57 +0300

    12787

--- projects/calebCurry/01Reference.cpp
+++ projects/calebCurry/01Reference.cpp
@@ -2,7 +2,7 @@
       Tutorial 1 and 2 - Working with References in C++
 https://www.youtube.com/watch?v=OCL7mSFCIx0&list=PL_c9BZzLwBRJkVDaJbLHrrjNH_phcbCy7
 https://www.youtube.com/watch?v=GVEcLAX7ogk&list=PL_c9BZzLwBRJkVDaJbLHrrjNH_phcbCy7&index=2   
-3:44
+5:34
       1.   save file
       2.  g++  01Reference.cpp  -o  refe  -std=c++11
       3.  ./refe
@@ -15,19 +15,49 @@ https://www.youtube.com/watch?v=GVEcLAX7ogk&list=PL_c9BZzLwBRJkVDaJbLHrrjNH_phcb
 
 #include <iostream>
  
-
+void work (int x){
+     x++;
+}
 int main(){
+
  int a =10;
- int& b =a;
- int c =100;
- std::cout <<"a =" <<a << std::endl <<"b ="  <<b <<   std::endl;
- std::cout <<"&a =" <<&a << std::endl <<"&b ="  <<&b <<   std::endl;
+ int& b = a; //Create a reference
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ std::cout <<"a =" <<a  <<" b ="  <<b <<   std::endl;
+ std::cout <<"&a =" <<&a  <<" &b =" <<&b <<   std::endl;
+ 
+ work ( a);
+ std::cout <<"\n\n  After work(); \n"<<"  --------\n"   ;
+ std::cout <<"a =" <<a  <<" b ="  <<b <<   std::endl;
+ std::cout <<"&a =" <<&a  <<" &b =" <<&b <<   std::endl;
+
+
+
+
+ // '&a'   is the address   of 'a' 
+ // int& b = a; //Create a reference. 
+                // b referes to the same location as a
+ 
+ 
+ 
  a = 15;
- std::cout <<"a = " <<a << std::endl <<"b = "  << b <<   std::endl;
+ std::cout <<"\n\n  After  a = 15; \n"<<"  --------\n"   ;
+ std::cout <<"a = " <<a  <<" b = "  << b <<   std::endl;
 
 
 
-
+int c =100;
+b =c;
+std::cout <<"\n\n  After b =c; \n"<<"  --------\n"   ;
+std::cout <<" a =" <<a  <<" b ="  <<b <<   std::endl;
+std::cout <<" &a =" <<&a  << " &b ="  <<&b <<   std::endl;
+ 
 
 
 
