23a4fb5928f8c060dfeacc8c19d77513a46e760f
danizalm05 <danizalm05@gmail.com>
2024-07-08 16:49:58 +0300

    17231

---
+++ projects/opencv/02BasicFunc.cpp
@@ -0,0 +1,37 @@
+/*   basic functio */
+//https://www.youtube.com/watch?v=2FYm3GOonhk  35:36
+//https://www.computervision.zone/courses/opencv-cv/
+//https://www.computervision.zone/topic/chapter-2-basic-functions-2/
+
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+///////////////  Basic Functions  //////////////////////
+
+void main() {
+
+	string path = "Resources/test.png";
+	Mat img = imread(path);
+	Mat imgGray, imgBlur, imgCanny, imgDil, imgErode;
+
+	cvtColor(img, imgGray, COLOR_BGR2GRAY);
+	GaussianBlur(imgGray, imgBlur, Size(7, 7), 5, 0);
+	Canny(imgBlur, imgCanny, 25, 75);
+
+	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+	dilate(imgCanny, imgDil, kernel);
+	erode(imgDil, imgErode, kernel);
+
+	imshow("Image", img);
+	imshow("Image Gray", imgGray);
+	imshow("Image Blur", imgBlur);
+	imshow("Image Canny", imgCanny);
+	imshow("Image Dilation", imgDil);
+	imshow("Image Erode", imgErode);
+	waitKey(0);
+}
--- projects/opencv/loadImage.cpp
+++ projects/opencv/loadImage.cpp
@@ -1,5 +1,5 @@
-/*Load image file*/
-//https://www.youtube.com/watch?v=2FYm3GOonhk  22:30
+/*   Load image file  */
+//https://www.youtube.com/watch?v=2FYm3GOonhk  22:30 35:36
 //https://www.computervision.zone/courses/opencv-cv/
 //https://www.computervision.zone/topic/chapter-1-read-images-videos-and-webcams/
 
@@ -10,6 +10,8 @@
 #include <opencv2/imgproc.hpp>
 #include <iostream>
 
+#include <windows.h>
+#include <Lmcons.h>
 using namespace cv;
 using namespace std;
 
@@ -18,11 +20,47 @@ using namespace std;
 
 void main() {
 
-    //string path = "Resources/test.png";
-    string path = "C:/Users/gilfm/Pictures/Saved Pictures/2.jpg";
+    
+    
+    //C:\Users\rockman\source\repos
+    
+    // string user_name = "";
+    string user_name = "rockman";
+    string image_name = "2.jpg";
+    string video_name = "1.mp4";
 
-    Mat img = imread(path);
-    imshow("Image", img);
-    waitKey(0);
+    string path = "C:/Users/" + user_name + "/Pictures/Saved Pictures/"  + image_name;
+    string video_path = "C:/Users/" + user_name + "/Videos/Captures/"+ video_name;
+ 
+    Mat img;
+    cout << video_path;
+    /*
+
+     img = imread(path);
+     imshow("Image", img);
+     waitKey(0);
+    */
+
+    ///////////////  Video or Webcam //////////////////////
+      
+     //VideoCapture cap(video_path); //use vidoe file
+    int Webcam_ID = 0;
+    VideoCapture cap(Webcam_ID);// use the Webcam
+       while (true) {
+           cap.read(img);
+     		imshow("Image", img);
+     		waitKey(2);
+    	}
+  
+
+
+    
+
+     
+
+
+
+
+
 
 }
