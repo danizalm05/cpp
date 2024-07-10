410fd98290ca27b7c8c2db0ce4aa19da54b1d560
danizalm05 <danizalm05@gmail.com>
2024-07-10 16:33:46 +0300

    21264

--- qt6/kdabyv/library/library/mainwindow.cpp
+++ qt6/kdabyv/library/library/mainwindow.cpp
@@ -1,6 +1,8 @@
 /*
- *Library Management System in Qt Creator with C++
+ *Library Management System in Qt  C++
+
 https://www.youtube.com/watch?v=FbyB4rkzE10&list=PL8MpAn684UHKt_umMjmD0EhbV5IZ-5uLd&index=5
+5:05
 */
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
