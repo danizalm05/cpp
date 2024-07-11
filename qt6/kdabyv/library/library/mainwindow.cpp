18ce7168a099894faa9008c65efe72cc88c553e8
danizalm05 <danizalm05@gmail.com>
2024-07-11 16:35:16 +0300

    8841

--- qt6/kdabyv/library/library/mainwindow.cpp
+++ qt6/kdabyv/library/library/mainwindow.cpp
@@ -2,7 +2,7 @@
  *Library Management System in Qt  C++
 
 https://www.youtube.com/watch?v=FbyB4rkzE10&list=PL8MpAn684UHKt_umMjmD0EhbV5IZ-5uLd&index=5
-10:05
+     24:05
 */
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
@@ -12,9 +12,28 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->OKpushButton,SIGNAL(clickd()),
+            this,SLOT(on_OKpushButton_clicked()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_OKpushButton_clicked()
+{
+    QString book[] = {"HP","Eng","Mat"};
+    int rollNo[] = {101,203,304};
+
+    QString ubook   = ui->BookNameLineEdit->text();
+    QString uRollNo = ui->RoleNumLineEdit->text();
+
+    int uRollNo_int =  uRollNo.toInt();
+
+    for (int i =0; i < sizeof(rollNo);i++)
+    {
+        if(uRollNo_int==rollNo[i]  &&  ubook==book[i] )
+    }
+}
+
