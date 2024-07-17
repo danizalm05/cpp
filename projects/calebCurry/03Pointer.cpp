568bc7984654560e5e798b49c1ee1644ae7af9cf
Richm <ric5hme45@yahoo.com>
2024-07-17 13:20:09 +0300

    25074

--- projects/calebCurry/03Pointer.cpp
+++ projects/calebCurry/03Pointer.cpp
@@ -1,7 +1,6 @@
- /*  3:30
+ /*  5:50
       Tutorial 4   Reference, Pointer Explained
- https://www.youtube.com/watch?v=u5AH2Wn6h1o&list=PL_c9BZzLwBRJkVDaJbLHrrjNH_phcbCy7&index=4
-      1. save file
+ https://www.youtube.com/watch?v=u5AH2Wn6h1o&list=PL_c9BZzLwBRJkVDaJbLHrrjNH_phcbCy7&index=4     1. save file
       2. compile:  g++  03Pointer.cpp  -o  poit  -std=c++11
       3. run:  ./poit
  ---------------------------
@@ -18,18 +17,32 @@ int main(){
 
  int x =5;
  int *y = &x;
+  
+ std::cout << " \n x = 5\n-----------" << std::endl;
+ std::cout <<"&x="<< &x <<" x="<<x <<" *y="<<*y<< std::endl;
+  //&x=0xa34d1ffbe4 x=5 *y=5
+ std::cout << "\n x = 6\n-----------" << std::endl;
+ x = 6;
+  //&x=0xa34d1ffbe4 x=6 *y=6
+ std::cout <<"&x="<< &x <<" x="<<x <<" *y="<<*y<< std::endl;
 
- std::cout << " \n\n x =5\n---------" << std::endl;
- 
- std::cout <<" &X = "<< &x << std::endl;
- std::cout <<"  X = "<<  x << std::endl;
- std::cout <<" *y = "<< *y << std::endl;
+std::cout << "\n *y = 7\n-----------" << std::endl;
+ *y = 7;
+ //x=0xc56f7ffd14 x=7 *y=7
+ std::cout <<"&x="<< &x <<" x="<<x <<" *y="<<*y<< std::endl;
 
- std::cout <<"\n change x to 6" << "\n---------" << std::endl;
- x =6;
- std::cout <<" &X = "<< &x << std::endl;
- std::cout <<"  X = "<<  x << std::endl;
- std::cout <<" *y = "<< *y << std::endl;
+std::cout << "\nz =10  y = &z\n-----------" << std::endl;
+ int z =10;
+ y = &z;
+ //&x=0xc56f7ffd14 x=7 *y=10
+ std::cout <<"&x="<< &x <<" x="<<x <<" *y="<<*y<< std::endl;
+
+
+std::cout << "\nz =10  y = &z\n-----------" << std::endl;
+ x =5;
+ *y = 60;
+ //&x=0xf3a4dffa44 x=5 *y=60
+ std::cout <<"&x="<< &x <<" x="<<x <<" *y="<<*y<< std::endl;
 
  return 0;
 }//main
