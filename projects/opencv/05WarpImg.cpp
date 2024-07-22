fb4aaf603610a77ac533f8037e2ae5ddea378421
danizalm05 <danizalm05@gmail.com>
2024-07-22 16:23:19 +0300

    29914

--- projects/opencv/05WarpImg.cpp
+++ projects/opencv/05WarpImg.cpp
@@ -2,6 +2,7 @@
 https://www.youtube.com/watch?v=2FYm3GOonhk  01:11:00  
 https://www.computervision.zone/courses/opencv-cv/ 
 https://www.computervision.zone/topic/chapter-5-warp-images/
+ https://github.com/murtazahassan/Learn-OpenCV-cpp-in-4-Hours
  */
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
@@ -23,7 +24,7 @@ void main() {
     //C:\Users\rockman\source\repos
 
     // string user_name = "gilfm"; "rockman";
-    string user_name = "gilfm";
+    string user_name = "rockman";//"gilfm";
     string image_name = "cards.jpg";//2.jpg";
     
 
@@ -36,7 +37,7 @@ void main() {
    Mat matrix, imgWarp;
    float w = 250, h = 350;
 
-   Point2f src[4] = { {529,142},{771,190},{405,395},{674,457} };
+   Point2f src[4] = { {529,142},{771,190},{405,395},{674,457} };//floating points values
    Point2f dst[4] = { {0.0f,0.0f},{w,0.0f},{0.0f,h},{w,h} };
 
    matrix = getPerspectiveTransform(src, dst);
