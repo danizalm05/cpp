95eff17d981bbce2b2f4fe73e2c424cef4074d88
Richm <ric5hme45@yahoo.com>
2024-08-07 12:37:21 +0300

    10757

---
+++ qt6/kdabyv/qobject04/qobject04/main.cpp
@@ -0,0 +1,21 @@
+/*
+/* Episode 4 - QObject and Moc VoidRealms
+
+https://www.youtube.com/watch?v=KugPAznC4Yo&list=PLUbFnGajtZlXbrbdlraCe3LMC_YH5abao&index=6
+
+https://github.com/voidrealms/qt6-core-beginners/blob/main/section%204/qt6cb-4-assign/main.cpp
+
+*/
+
+
+
+
+
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    return a.exec();
+}
