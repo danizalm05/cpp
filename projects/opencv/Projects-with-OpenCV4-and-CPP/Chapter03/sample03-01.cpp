7ff0b77a1e4409a75d966ef8355ecd89040ca48e
danizalm05 <danizalm05@gmail.com>
2024-08-17 16:21:56 +0300

    25405

--- projects/opencv/Projects-with-OpenCV4-and-CPP/Chapter03/sample03-01.cpp
+++ projects/opencv/Projects-with-OpenCV4-and-CPP/Chapter03/sample03-01.cpp
@@ -2,7 +2,7 @@
 * chapter 3/ sample1
  Building Computer Vision Projects with OpenCV 4 and C++
  github.com/PacktPublishing/Building-Computer-Vision-Projects-with-OpenCV4-and-CPlusPlus/blob/master/Chapter03
- page 105
+ page 105- 111
  */
 
 
@@ -23,7 +23,6 @@ using namespace cv;
 using namespace std;
 
 
-/////////////////  Images  //////////////////////
 
 int main() {
 
@@ -32,9 +31,9 @@ int main() {
     //C:\Users\rockman\source\repos
 
     // string user_name = "gilfm"; "rockman";
-    string user_name = "gilfm";
-    string image_name = "lena.jpg";// "2.jpg";
-    string image_name2 = "road.jpg"; 
+    string user_name = "rockman";
+    string image_name = "lena.jpg"; 
+    string image_name2 = "photo.jpg"; 
 
     string path = "C:/Users/" + user_name + "/Pictures/Saved Pictures/" + image_name;
     string path2 = "C:/Users/" + user_name + "/Pictures/Saved Pictures/" + image_name2;
@@ -49,26 +48,53 @@ int main() {
 
    
     Mat gray = imread(path, IMREAD_GRAYSCALE);
-
+    namedWindow("Lena", WINDOW_NORMAL);//allows resize 
     if (!lena.data) // Check for invalid input
     {
         cout << "Could not open or find the image" << std::endl;
         return -1;
     }
-    // Write images
-    imwrite("lenaGray.jpg", gray);
-
+    namedWindow("Photo", WINDOW_AUTOSIZE);//automatically adjusted
+    if (!photo.data) {
+        cout << "Photo.jpg image missing!" << endl;
+        return -1;
+    }
+    // Move window
+    moveWindow("Lena", 10, 10);
+    moveWindow("Photo", 520, 10);
     // Get same pixel with opencv function
-    int myRow = lena.cols - 1;
-    int myCol = lena.rows - 1;
+    int myRow = lena.cols - 4;
+    int myCol = lena.rows - 4;
     auto pixel = lena.at<Vec3b>(myRow, myCol);
-    cout << "Pixel value (B,G,R): (" << (int)pixel[0] << "," << (int)pixel[1] << "," << (int)pixel[2] << ")" << endl;
+    cout << "Pixel: (" << (int)pixel[0] << "," << (int)pixel[1] << "," << (int)pixel[2] << ")" << endl;
 
     // show images
-    imshow("Lena BGR", lena);
-    imshow("Lena Gray", gray);
+    imshow("Lena", lena);
+    imshow("Photo", photo);
+    resizeWindow("Lena", 512, 512);
+
     // wait for any key press
     waitKey(0);
+
+    // Destroy the windows
+    destroyWindow("Lena");
+    destroyWindow("Photo");
+    // wait for any key press
+    waitKey(0);
+
+    // Create 10 windows
+    for (int i = 0; i < 10; i++)
+    {
+        ostringstream ss;
+        ss << "Photo " << i;
+        namedWindow(ss.str());
+        moveWindow(ss.str(), 20 * i, 20 * i);
+        imshow(ss.str(), photo);
+    }
+
+    waitKey(0);
+    // Destroy all windows
+    destroyAllWindows();
     return 0;
 
 
