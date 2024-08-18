5929f50cd26491420ee8f7cf410d2ef9830f4c35
Richm <ric5hme45@yahoo.com>
2024-08-18 13:10:46 +0300

    7957

--- projects/opencv/Projects-with-OpenCV4-and-CPP/Chapter03/sample03-03.cpp
+++ projects/opencv/Projects-with-OpenCV4-and-CPP/Chapter03/sample03-03.cpp
@@ -1,79 +1,105 @@
+/*
+* chapter 3/ sample3
+ Building Computer Vision Projects with OpenCV 4 and C++
+ github.com/PacktPublishing/Building-Computer-Vision-Projects-with-OpenCV4-and-CPlusPlus/blob/master/Chapter03
+ page 112
+ */
+
+
+ // OpenCV includes
+#include "opencv2/core.hpp"
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <iostream>
-#include <string>
-#include <sstream>
+
+
+
+
+
+#include <windows.h>
+#include <Lmcons.h>
+using namespace cv;
 using namespace std;
 
-// OpenCV includes
-#include "opencv2/core.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/highgui.hpp"
-using namespace cv;
 
-// Create a variable to save the position value in track
-int blurAmount=15;
+int blurAmount = 15;
 
 // Trackbar call back function
 static void onChange(int pos, void* userInput);
 
 //Mouse callback
-static void onMouse( int event, int x, int y, int, void* userInput );
+static void onMouse(int event, int x, int y, int, void* userInput);
+int main() {
 
-int main( int argc, const char** argv )
-{
-	// Read images
-	Mat lena= imread("../lena.jpg");
-	
-	// Create windows
-	namedWindow("Lena");
-	
-	// create a trackbark
-	createTrackbar("Lena", "Lena", &blurAmount, 30, onChange, &lena);
-	
-	setMouseCallback("Lena", onMouse, &lena);
 
-	// Call to onChange to init
-	onChange(blurAmount, &lena);
-		
-	// wait app for a key to exit
-	waitKey(0);
-	
-	// Destroy the windows
-	destroyWindow("Lena");
-	
-	return 0;
+
+    //C:\Users\rockman\source\repos
+
+    // string user_name = "gilfm"; "rockman";
+    string user_name = "gilfm";
+    string image_name = "lena.jpg";// "2.jpg";
+    string path = "C:/Users/" + user_name + "/Pictures/Saved Pictures/" + image_name;
+     
+
+    Mat img;
+    cout << path;
+
+    
+    // Read images
+    Mat lena = imread(path);
+    namedWindow("Lena");
+    imshow("Lena", lena);
+     
+    if (!lena.data) // Check for invalid input
+    {
+        cout << "Could not open or find the image" << std::endl;
+        return -1;
+    }
+    createTrackbar("Lena", "Lena", &blurAmount, 30, onChange, &lena);
+
+    setMouseCallback("Lena", onMouse, &lena);
+    onChange(blurAmount, &lena);
+    
+    //imshow("Lena", lena);
+     
+    // wait for any key press
+    waitKey(0);
+    return 0;
 }
+
 
 // Trackbar call back function
 static void onChange(int pos, void* userInput)
 {
-	if(pos <= 0)
-		return;
-	// Aux variable for result
-	Mat imgBlur;
+    if (pos <= 0)
+        return;
+    // Aux variable for result
+    Mat imgBlur;
 
-	// Get the pointer input image
-	Mat* img= (Mat*)userInput;
+    // Get the pointer input image
+    Mat* img = (Mat*)userInput;
 
-	// Apply a blur filter
-	blur(*img, imgBlur, Size(pos, pos));	
+    // Apply a blur filter
+    blur(*img, imgBlur, Size(pos, pos));
 
-	// Show the result
-	imshow("Lena", imgBlur);
+    // Show the result
+    imshow("Lena", imgBlur);
 }
 
 //Mouse callback
-static void onMouse( int event, int x, int y, int, void* userInput )
+static void onMouse(int event, int x, int y, int, void* userInput)
 {
-	if( event != EVENT_LBUTTONDOWN )
-	        return;
+    if (event != EVENT_LBUTTONDOWN)
+        return;
 
-	// Get the pointer input image
-	Mat* img= (Mat*)userInput;
-	
-	// Draw circle
-	circle(*img, Point(x, y), 10, Scalar(0,255,0), 3);
+    // Get the pointer input image
+    Mat* img = (Mat*)userInput;
 
-	// Call on change to get blurred image
-	onChange(blurAmount, img);
+    // Draw circle
+    circle(*img, Point(x, y), 10, Scalar(0, 255, 0), 3);
+
+    // Call on change to get blurred image
+     onChange(blurAmount, img);
 
 }
