e4cf27ff8c95cb4980b813ab6d9fc8cfc795592f
Richm <ric5hme45@yahoo.com>
2024-09-22 12:30:37 +0300

    12031

--- projects/opencv/docsOpencv/mat.cpp
+++ projects/opencv/docsOpencv/mat.cpp
@@ -1,14 +1,14 @@
- /*
+/*
 Mat - The Basic Image Container
  how OpenCV stores and handles images.
 https://docs.opencv.org/4.x/de/d7a/tutorial_table_of_content_core.html
 https://docs.opencv.org/4.x/d6/d6d/tutorial_mat_the_basic_image_container.html
 
-Mat is a class with two data parts: 
-1. the matrix header  
-2. pointer to the matrix containing the pixel values 
- . 
-The size of the matrix itself may vary from image to image 
+Mat is a class with two data parts:
+1. the matrix header
+2. pointer to the matrix containing the pixel values
+ .
+The size of the matrix itself may vary from image to image
 */
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
@@ -16,12 +16,12 @@ The size of the matrix itself may vary from image to image
 
 #include <iostream>
 
- //C:\Users\rockman\source\repos
+//C:\Users\rockman\source\repos
 
-    // string user_name = "gilfm"; "rockman";
-std::string user_name = "rockman";
-std::string image_name = "2.jpg";
- 
+   // string user_name = "gilfm"; "rockman";
+std::string user_name = "gilfm";
+std::string image_name = "1.jpg";
+
 
 std::string path = "C:/Users/" + user_name + "/Pictures/Saved Pictures/";
 std::string image_path = path + image_name;
@@ -30,20 +30,20 @@ std::string image_path = path + image_name;
 int main()
 {
     //std::string image_path = samples::findFile("starry_night.jpg");
-    
+
     cv::Mat A, C;   // creates just the header parts
-    A =  cv::imread(image_path, cv::IMREAD_COLOR);
+    A = cv::imread(image_path, cv::IMREAD_COLOR);
 
     if (A.empty())
     {
         std::cout << "Could not read the image: " << image_path << std::endl;
         return 1;
-     }
+    }
     /*
     All the next A,B,C objects, point to the same single data matrix and making a modification using
-    any of them will affect all the other ones. In practice the different objects just provide 
+    any of them will affect all the other ones. In practice the different objects just provide
     different access methods to the same underlying data.
-    But, their header parts are different. 
+    But, their header parts are different.
      */
     cv::imshow("A mat", A);
     cv::Mat B(A);                     // Use the copy constructor
@@ -51,22 +51,50 @@ int main()
     C = A;                            // Assignment operator   only copy the header.
     cv::imshow(" C mat", C);
 
-    int k = cv::waitKey(0); // Wait for a keystroke in the window 
+    //int k = cv::waitKey(0); // Wait for a keystroke in the window 
 
     //ROI (region of interest)  Create a new header with the new boundaries:
     cv::Mat D(A, cv::Rect(10, 10, 300, 300)); // using a rectangle
     cv::Mat E = A(cv::Range::all(), cv::Range(1, 415)); // using row and column boundaries
-    cv::imshow(" Rect(10, 10, 300, 300)",D);
-    cv::imshow(" Range::all(), cv::Range(1,415)",E);
-  
+    cv::imshow(" Rect(10, 10, 300, 300)", D);
+    cv::imshow(" Range::all(), cv::Range(1,415)", E);
+
     //copy the matrix itself 
     // modifying F or G will not affect the matrix pointed to by the A's header
     cv::Mat F = A.clone();
     cv::Mat G;
     A.copyTo(G);
 
-    cv::waitKey(0);
+  
 
-    //Storing methods
+    cv::Mat M(2, 2, CV_8UC3, cv::Scalar(0, 0, 255));
+    /*
+    * M =
+        [  0,   0, 255,   0,   0, 255;
+           0,   0, 255,   0,   0, 255     ]
+     Two dimensional and multichannel images:
+     Specify the data type to use for storing the elements 
+     and the number of channels per matrix point. 
+     To do this we have multiple definitions constructed 
+     according to the following convention:
+    CV_[The number of bits per item]
+       [Signed or Unsigned]
+       [Type Prefix]
+       C[The channel number]
+   CV_8UC3 means we use unsigned char types 
+    8 bit long and each pixel has three of these to 
+   form the three channels. 
+    
+   The cv::Scalar is four element short vector. 
+   Specify it and you can initialize all matrix points with a 
+   custom value. 
+   
+    
+    */
+   
+   M.create(4,4, CV_8UC(2));//4X4 each item 2 digit 
+   std::cout << "M = " << std::endl << " " << M << std::endl << std::endl;
+
+    cv::waitKey(0);
     return 0;
 }
